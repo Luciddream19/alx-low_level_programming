f0a9704981f5735d0b6da757e0172d90c6f65cbc
Luciddream19 <mbama6@gmail.com>
2023-06-16 17:54:42 -0700

    the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  *
  * This function serves as the entry point for the program execution.
  * It generates a random number, determines whether it is greater than 5,
- * equal to zero, or less than 6 but not equal to zero, and prints the last digit of the number.
+ * is zero or less than 6 but not zero, and prints the last digit of the number.
  *
  * Return: Always 0 (success)
  */
@@ -17,19 +17,18 @@ int n, last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-last_digit = abs(n % 10);
- 
+last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d, and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("Last digit of %d is %d, and is equal to 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %d, and is less than 6 and not equal to 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 
 return (0);
